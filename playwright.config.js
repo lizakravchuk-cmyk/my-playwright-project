ed8c9626a7d5d6f86747f324f82ee0c7b74d0c24
Yelyzaveta Kravchuk <lizacherchenkov@gmail.com>
2026-07-07 22:35:34 +0200

    add playwright tests for module 2

---
+++ playwright.config.js
@@ -0,0 +1,27 @@
+import { defineConfig, devices } from '@playwright/test';
+
+export default defineConfig({
+  testDir: './tests',
+  fullyParallel: true,
+  forbidOnly: !!process.env.CI,
+  retries: 2,
+  workers: 2,
+  reporter: 'html',
+  use: {
+    headless: true,
+    trace: 'on-first-retry',
+    screenshot: 'only-on-failure',
+    video: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+    {
+      name: 'firefox',
+      use: { ...devices['Desktop Firefox'] },
+    },
+   
+  ],
+});
---
+++ tests/example.spec.js
@@ -0,0 +1,20 @@
+import { test, expect } from '@playwright/test';
+
+test.describe('My First Test Suite', () => {
+
+  test.use({storageState: 'state.json'});
+
+  test.beforeEach(async ({ page }) => {
+    await page.goto('https://www.epam.com/', { waitUntil: 'domcontentloaded' });
+  });
+
+  test('should open https://www.epam.com/ page', async ({ page }) => {
+    await expect(page).toHaveTitle(/EPAM/, { timeout: 30000 });
+  });
+
+  test('should hamburger menu', async ({ page }) => {
+    await page.locator('button.hamburger-menu__button').click();
+    await expect(page.locator('div.hamburger-menu__dropdown-section')).toBeVisible();
+  });
+
+})
